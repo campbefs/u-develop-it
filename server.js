ef0c97c524a9de700dce3f381f5842b852a4dd3a
Christopher Campbell <campbefs@gmail.com>
2021-04-28 22:06:52 -0700

    sql callback function

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 app.get('/', (req, res) => {
   res.json({
@@ -12,11 +28,13 @@ app.get('/', (req, res) => {
   });
 });
 
+
+
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 })
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
